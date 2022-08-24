b8050d7bfddcb4529f4e7de473094c35a1a796bf
Dmitry Shokhov <dshohov@gmail.com>
2022-08-24 22:23:52 +0300

    11 commit

--- RunGroopWebApp/Controllers/ClubController.cs
+++ RunGroopWebApp/Controllers/ClubController.cs
@@ -119,5 +119,21 @@ namespace RunGroopWebApp.Controllers
 
             return RedirectToAction("Index");
         }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var clubDetails = await _clubRepository.GetByIdAsync(id);
+            if(clubDetails == null) return View("Error");
+            return View(clubDetails);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteClub(int id)
+        {
+            var clubDetails = await _clubRepository.GetByIdAsync(id);
+            if (clubDetails == null) return View("Error");
+
+            _clubRepository.Delete(clubDetails);
+            return RedirectToAction("Index");
+        }
     }
 }
--- RunGroopWebApp/Controllers/RaceController.cs
+++ RunGroopWebApp/Controllers/RaceController.cs
@@ -117,5 +117,20 @@ namespace RunGroopWebApp.Controllers
 
             return RedirectToAction("Index");
         }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var raceDetails = await _raceRepository.GetByIdAsync(id);
+            if (raceDetails == null) return View("Error");
+            return View(raceDetails);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteClub(int id)
+        {
+            var raceDetails = await _raceRepository.GetByIdAsync(id);
+            if (raceDetails == null) return View("Error");
+
+            _raceRepository.Delete(raceDetails);
+            return RedirectToAction("Index");
+        }
     }
 }
